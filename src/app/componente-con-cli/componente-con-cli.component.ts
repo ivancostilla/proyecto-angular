a58473e1825a4cf84f5da95c20fb3fa95eca251b
ivancostilla <ivan20punto88@gmail.com>
2021-06-28 23:58:44 -0300

    interpolación

--- src/app/componente-con-cli/componente-con-cli.component.ts
+++ src/app/componente-con-cli/componente-con-cli.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 //con cli usar ng g c nombre... para un nuevo componente
 @Component({
   selector: 'app-componente-con-cli',
@@ -7,6 +7,16 @@ import { Component} from '@angular/core';
 })
 export class ComponenteConCLIComponent {
 
-  constructor() { }
+  nombre: string = 'Juan';
+  apellido: string = 'Perez';
+  private edad: number = 29;
 
+  personasArray = [
+    {nombre:'iván',apellido: 'costilla',edad: 29},
+    {nombre: 'pepe', apellido: 'juarez', edad: 30}
+  ]
+  //metodo para usar atributos privados
+  getEdad(): number{
+    return this.edad;
+  }
 }
