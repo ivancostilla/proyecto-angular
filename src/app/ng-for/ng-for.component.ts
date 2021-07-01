f3332d19c5f806881bb2867a3ec2faa93ce05855
ivancostilla <ivan20punto88@gmail.com>
2021-07-01 04:16:35 -0300

    agregado dinamico de elementos a un objeto

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -9,9 +9,19 @@ import { Persona } from './persona.model';
 export class NgForComponent {
 
 titulo: string = 'Listado de Personas';
+nombreInput: string = '';
+apellidoInput: string = '';
+
 personas: Persona[] = [
   new Persona('Juan','Pérez'),
   new Persona('Laura', 'Gómez'),
   new Persona('Lolo', 'Zapata')
 ]
+
+agregarPersona(){
+  let persona1 = new Persona(this.nombreInput,this.apellidoInput)
+  //dos formas de agregar dinamicamente 1 persona:
+  // this.personas.push( persona1 );
+  this.personas = [...this.personas, persona1]
+}
 }
